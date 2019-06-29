06d64af6fccdfdca5c9f3ced23096a45789f4bdd
luis-k0-pearljam <luisk.net@gmail.com>
2019-06-29 19:52:17 -0300

    lesson 71

--- android/app/src/main/java/com/awesomeproject/MainActivity.java
+++ android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -1,15 +1,19 @@
 package com.awesomeproject;
 
-import com.facebook.react.ReactActivity;
+// react-native-navigation
+// import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.controllers.SplashActivity;
 
-public class MainActivity extends ReactActivity {
+// react-native-navigation
+// public class MainActivity extends ReactActivity {
+public class MainActivity extends SplashActivity  {
 
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
-    @Override
-    protected String getMainComponentName() {
-        return "AwesomeProject";
-    }
+    // @Override
+    // protected String getMainComponentName() {
+    //     return "AwesomeProject";
+    // }
 }
