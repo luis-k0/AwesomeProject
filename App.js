9001291117766a03658f1e908e593863a71e3ec3
luis-k0-pearljam <luisk.net@gmail.com>
2019-07-02 14:40:20 -0300

    lesson 79

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
+import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail';
 import configureStore from './src/store/configureStore';
 
 const store = configureStore();
@@ -24,6 +25,12 @@ Navigation.registerComponent(
 Navigation.registerComponent(
   'awesome-places.FindPlaceScreen',
   () => FindPlaceScreen,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  'awesome-places.PlaceDetailScreen',
+  () => PlaceDetailScreen,
   store,
   Provider
 );
